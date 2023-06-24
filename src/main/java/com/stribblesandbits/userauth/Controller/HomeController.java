75e81d2dbe2a24b65e5d75630b0795716a656519
chance-stribling <cstribling1717@gmail.com>
2023-06-23 20:08:28 -0400

    init commit

---
+++ src/main/java/com/stribblesandbits/userauth/Controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.stribblesandbits.userauth.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        return "Hello World!";
+    }
+    @GetMapping("/secured")
+    public String secured(){
+        return "Hello! SECURED.";
+    }
+}
